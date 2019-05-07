cad0d9257871236d384698a2ae7a4f94ab693e7d
Chiboba <briangfelix@gmail.com>
2019-05-06 18:16:06 -0700

    More styling for requests page, also added create request button/popup and styled that. Added notes for further additions needed for requests page

--- myhour-fe/src/components/Requests/Request.tsx
+++ myhour-fe/src/components/Requests/Request.tsx
@@ -17,7 +17,7 @@ const Request = observer((props:any) => {
                     <img src={props.props.image ? '' : avatar}></img>
                 </div>
                 <div className ='request-content'>
-                    
+                    <span>{props.props.userName}</span>
                     <span>{props.props.position}</span>
                     <span>{props.props.description}</span>
                     <span>{props.props.startTime} - {props.props.endTime}</span>
--- myhour-fe/src/components/Requests/RequestsListPage.tsx
+++ myhour-fe/src/components/Requests/RequestsListPage.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {observer} from 'mobx-react-lite';
 import './Requests.css';
 import Request from './Request';
+import { number } from 'prop-types';
+
+/*NOTES:
+Important:
+    - Need to make it so someone can't add a shift to their schedule if they already have a shift at that time.
+    - Possibly need a way to limit how many hours someone can work a day.
+    - Need a clickaway for the create request.
+    - Need a popup for the accept button, to double confirm they want to take the shift. Also needs clickaway.
+    - Need to make it so urgen requests always show up first.
+For Later: 
+    - Messages should open up the conversation tab with the ability to message the person of interest.
+*/
 
 const RequestListPage = observer(() => {
 
@@ -35,12 +47,69 @@ const RequestListPage = observer(() => {
         },
     ]
 
+    //Want to get the shifts for this person for the specified day
+    const tempShifts = [
+        {
+            id: 1,
+            information: '12:30PM - 4:30PM'
+        },
+        {
+            id: 2,
+            information: '7:30AM - 9:00AM'
+        }
+    ]
+
+    interface RequestContent {
+        message: string;
+        shiftID: number;
+    }
+
+    const [creatingRequest, setCreatingRequest] = useState(false);
+    const [requestContent, setRequestContent] = useState<RequestContent>({message: '', shiftID: 0})
+
+    const handlePost = () => {
+        setCreatingRequest(false);
+    }
+
+    useEffect(() => {
+      return (
+        setRequestContent({...requestContent, shiftID: tempShifts[0].id})
+      );
+    }, [])
+
+    console.log(requestContent)
     return (
-        <div className='request-list-wrapper'>    
+        <div className='request-list-wrapper'>
+            {creatingRequest ? 
+                <div className='request-creator-wrapper'>
+                    <h2>Request Creator</h2>
+                    <div className='creator-content'>
+                        <textarea 
+                            maxLength={200}
+                            placeholder='Could someone please take over this shift for me?'
+                            onChange={(ev) => {setRequestContent({...requestContent, message: ev.target.value})}}
+                        />
+                        <div className="myselect">
+                            <select className="form-control" id="test" onChange={(ev) => {
+                                console.log(ev.target.value);
+                                setRequestContent({...requestContent, shiftID: +ev.target.value})}}>
+                                {tempShifts.map(shift =>
+                                    <option value={shift.id}>{shift.information}</option>
+                                )}
+                        </select>
+                        </div>
+                    </div>
+                    <div className='creator-buttons'>
+                        <button onClick={() => {handlePost()}} className="green">Post</button>
+                        <button onClick={() => {setCreatingRequest(false)}} className="red">Cancel</button>
+                    </div>
+                </div>
+                : ''}
             <div className='header'>
                 <h1>Requests</h1>
-                <div>
-
+                <div onClick={() => {setCreatingRequest(true)}} className='addButton'>
+                    <span className='createPlus'>+ </span>
+                    <span className='createContent'>Create Request</span>
                 </div>
             </div>
             <div className='create-request-wrapper'>
