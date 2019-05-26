cf843e2b181916fc91b79cbbdac57e65cd634fe0
Chiboba <briangfelix@gmail.com>
2019-05-26 15:49:54 -0700

    Added warning/error messages for each input, small stlying error with password view buttons that will be fixed next update, still need to wire it up with the web api

--- myhour-fe/src/components/Authentication/Signup.tsx
+++ myhour-fe/src/components/Authentication/Signup.tsx
@@ -14,9 +14,10 @@ import isMobilePhone from 'validator/lib/isMobilePhone';
 const Signup = observer((props:any) => {
     
     const state = useContext(GlobalStateContext);
-    const [registerInfo, setRegisterInfo] = useState({Firstname: '', Lastname: '', "email": '', Password: '', ConfirmPassword: '', CompanyName: '', CompanyNumber: ''});
+    const [registerInfo, setRegisterInfo] = useState({Firstname: '', Lastname: '', email: '', Password: '', ConfirmPassword: '', CompanyName: '', CompanyNumber: ''});
     const [viewPass, setViewPass] = useState(false);
     const [viewPass2, setViewPass2] = useState(false);
+    const [activeErrors, setActiveErrors] = useState({email: true, Firstname: false, Lastname: false, Password: false, ConfirmPassword: false, CompanyNumber: true, CompanyName: false})
 
     const attemptRegister = () => {
         const {email, Firstname, Lastname, Password, ConfirmPassword, CompanyName, CompanyNumber} = registerInfo;
@@ -24,11 +25,11 @@ const Signup = observer((props:any) => {
         let newActive = {email: isEmail(email), 
             Firstname: Firstname.length === 0, 
             Lastname: Lastname.length === 0, 
-            Password: Password.length < 7 && Password != ConfirmPassword, 
+            Password: Password.length < 7, 
             ConfirmPassword: Password != ConfirmPassword, 
             CompanyName: CompanyName.length === 0, 
             CompanyNumber: isMobilePhone(CompanyNumber.replace(/-/g, ""), 'any')}
-            console.log(newActive);
+        setActiveErrors(newActive)
         if(JSON.stringify(safeActive) === JSON.stringify(newActive)) {
             console.log('PASSED');
         }
@@ -40,28 +41,36 @@ const Signup = observer((props:any) => {
         <div className='signUpWrapper'>
                 <h1>Business Registration</h1>
             <div className='signUpContainer'>
+                <h5>Note: If you are an employee, request your account information from your manager or similar.</h5>
                 <div className='inputWrapper'> 
                         <label>FIRST NAME *</label>    
                         <input className="nameInput" placeholder='ex. Jennifer' onChange={(ev) => { setRegisterInfo({...registerInfo, Firstname: ev.target.value})}}/>
+                        {activeErrors.Firstname ? <p>Please include a first name.</p> : null}
                         <label>LAST NAME *</label>    
                         <input className="nameInput" placeholder='ex. Smith' onChange={(ev) => { setRegisterInfo({...registerInfo, Lastname: ev.target.value})}}/>
+                        {activeErrors.Lastname ? <p>Please include a last name.</p> : null}
                         <label>CONTACT EMAIL *</label>    
                         <input className="emailInput" placeholder='ex. jenniferSmith@gmail.com' onChange={(ev) => { setRegisterInfo({...registerInfo, email: ev.target.value})}}/>
+                        {!activeErrors.email ? <p>Please include a valid email address.</p> : null}
                         <label>CONTACT NUMBER *</label>    
                         <input className="contactInput" placeholder='ex. 123-123-1234' onChange={(ev) => { setRegisterInfo({...registerInfo, CompanyNumber: ev.target.value})}}/>
+                        {!activeErrors.CompanyNumber ? <p>Please include a valid phone number.</p> : null}
                         <label>ORGANIZATION NAME *</label>    
                         <input className="companyInput" placeholder="ex. Jennifer's Coffee" onChange={(ev) => { setRegisterInfo({...registerInfo, CompanyName: ev.target.value})}}/>
+                        {activeErrors.CompanyName ? <p>Please include an organization name.</p> : null}
                     <div className="passwordInput">
                         <label>PASSWORD *</label> 
                         <input type={ viewPass ? 'text' : 'password'} placeholder='Enter your Password' onChange={(ev) => { setRegisterInfo({...registerInfo, Password: ev.target.value})}}/>
                         <div onClick={() => {setViewPass(!viewPass)}} className='toggleEye'>
                             <FontAwesomeIcon style={{color: (viewPass ? '#60B0F4' : '')}} icon={faEye} />
                         </div>
+                        {activeErrors.Password ? <p>Password too short. Must be minimum 7 characters.</p> : null}
                         <label>CONFIRM PASSWORD *</label> 
                         <input type={ viewPass2 ? 'text' : 'password'} placeholder='Confirm your Password' onChange={(ev) => { setRegisterInfo({...registerInfo, ConfirmPassword: ev.target.value})}}/>
                         <div onClick={() => {setViewPass2(!viewPass2)}} className='toggleEye'>
                             <FontAwesomeIcon style={{color: (viewPass2 ? '#60B0F4' : '')}} icon={faEye} />
                         </div>
+                        {activeErrors.ConfirmPassword ? <p>Passwords do not match.</p> : null}
                     </div>
                 </div>
                 <div className='registerButtons'>
